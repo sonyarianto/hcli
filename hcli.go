cd79a6053e2b13d91fea1f70f075a7bbc047b9cf
Sony AK <sony@sony-ak.com>
2023-01-30 00:25:19 +0700

    Add version and credit things

--- hcli.go
+++ hcli.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"fmt"
+
 	"hcli/cli"
 )
 
+const (
+	version   = "0.1.0"
+	author    = "Sony AK <sony@sony-ak.com>"
+	shortName = "hcli"
+	longName  = "Handly CLI"
+)
+
 func main() {
+	fmt.Println(longName + " v" + version + " by " + author + "\n")
 	cli.DetectAvailableCli()
 }
